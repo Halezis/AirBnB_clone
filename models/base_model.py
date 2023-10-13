1b6354d60459f02e6bfae53c93293deb8f04d8f2
Halezis <halezis007@gmail.com>
2023-10-13 16:19:34 +0100

    base model commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,57 @@
+import uuid
+from datetime import datetime
+
 class BaseModel:
-	"""
-	This is a base class thattakes care of initiaiztion, seri all our future initi
+    """
+	This is a base class that takes care of initiaiztion, serialization
+	and deserialization of our instances
+    """
+
+    def __init__ (self, id, created_at, updated_at):
+        """
+        Constructor to initialize public instance attributes
+
+        id: a unique identifier for each instance
+        created_at: the datetime when an instance is created
+        updated_at: the datetime each time an object is changed
+        """
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        A method in the class that prints in the desired format
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict()}"
+
+    def save(self):
+        """
+        A public instance method that updates 'updated_at' with the
+        current datetime
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        A p.i.m that returns a dictionary containing all keys/values of
+        '__dict__' of the instance
+
+        return: as stated
+        """
+
+        #converting the datetimes to ISO format
+        created_at_iso = self.created_at.isoformat()
+        updated_at_iso = self.updated_at.isoformat()
+
+        #creating a dictionary
+        obj_dict = {
+        "__class__": self.__class__.__name__,
+        "created_at": created_at_iso,
+        "updated_at": updated_at_iso,
+        **self.__dict__
+        }
+
+        return obj_dict
